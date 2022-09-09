32a8a8d984b397f572a09872ad28ffb5990966d5
Marcos8123 <113150825+Marcos8123@users.noreply.github.com>
2022-09-08 19:26:35 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,27 @@
+function Episodio1(){
+window.open("Episodio1.html")
+}
+function Episodio2(){
+window.open("Episodio2.html")
+}
+function Episodio3(){
+window.open("Episodio3.html")
+}
+function Episodio4(){
+window.open("Episodio4.html")
+}
+function Episodio5(){
+window.open("Episodio5.html")
+}
+function Episodio6(){
+window.open("Episodio6.html")
+}
+function Episodio7(){
+window.open("Episodio7.html")
+}
+function Episodio8(){
+window.open("Episodio8.html")
+}
+function Episodio9(){
+window.open("Episodio9.html")
+}
